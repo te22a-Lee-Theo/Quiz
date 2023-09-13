728a4ecfcc751a690cb3979a497db3fe90338ea9
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-13 12:17:58 +0200

    fråga 1

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -2,7 +2,7 @@
 
 // string sum = "2" + p;
 
-int sum = 20 + 6;
+int sum = 0;
 
 // int p = 5;
 // sum = sum + p;
@@ -13,7 +13,33 @@ int sum = 20 + 6;
 // sum--;
 // minska med 1
 
-sum = 6/2;
+Console.WriteLine("Fråga 1 av 3.\nVad är banan klassificerad som? 1. Grönsak  2. Bär  3. Frukt");
+string  number = Console.ReadLine();
+int num = int.Parse(number);
+
+if (num == 1)
+{
+    Console.WriteLine("Fel svar, ingen poäng.");
+}
+
+else if (num == 2)
+{
+    Console.WriteLine("Rätt svar, en poäng.");
+    sum ++;
+}
+
+else if (num == 3)
+{
+    Console.WriteLine("Fel svar, ingen poäng.");
+}
+
+else
+{
+    Console.WriteLine("Invalid Response");
+}
+
+
+
 
 Console.WriteLine($"summan blir {sum}");
 
