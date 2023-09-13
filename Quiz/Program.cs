1ad75ed93875b42768c3f2a5fef36ac45db6f2db
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-13 10:39:15 +0200

    ujbhöouiöuo

---
+++ Quiz/Program.cs
@@ -0,0 +1,25 @@
+﻿// string p = "5";
+
+// string sum = "2" + p;
+
+int sum = 20 + 6;
+
+// int p = 5;
+// sum = sum + p;
+
+// sum++;
+// öka med 1
+
+// sum--;
+// minska med 1
+
+sum = 6/2;
+
+Console.WriteLine($"summan blir {sum}");
+
+if (sum > 3)
+{
+    Console.WriteLine("yay");
+}
+
+Console.ReadLine();
