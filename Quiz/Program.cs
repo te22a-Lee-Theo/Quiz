b669026e7cf4eab8acf044bf74d217ba475f9ddd
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-18 09:08:50 +0200

    fråga 3

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -19,23 +19,23 @@ int num = int.Parse(number);
 
 if (num == 1)
 {
-    Console.WriteLine("Fel svar, ingen poäng.");
+    Console.WriteLine("Fel svar, ingen poäng.\n");
 }
 
 else if (num == 2)
 {
-    Console.WriteLine("Rätt svar, en poäng.");
+    Console.WriteLine("Rätt svar, en poäng.\n");
     sum ++;
 }
 
 else if (num == 3)
 {
-    Console.WriteLine("Fel svar, ingen poäng.");
+    Console.WriteLine("Fel svar, ingen poäng.\n");
 }
 
 else
 {
-    Console.WriteLine("Invalid Response");
+    Console.WriteLine("Invalid Response\n");
 }
 
 
@@ -45,27 +45,50 @@ int was = int.Parse(wasd);
 
 if (was == 1)
 {
-    Console.WriteLine("Rätt svar, en poäng.");
+    Console.WriteLine("Rätt svar, en poäng.\n");
     sum ++;
 }
 else if (was == 2)
 {
-    Console.WriteLine("Fel svar, ingen poäng.");
+    Console.WriteLine("Fel svar, ingen poäng.\n");
 }
 else if (was == 3)
 {
-    Console.WriteLine("Fel svar ingen poäng");
+    Console.WriteLine("Fel svar ingen poäng.\n");
 }
 else
 {
-    Console.WriteLine("Invalid Response");
+    Console.WriteLine("Invalid Response\n");
+}
+
+
+Console.WriteLine("Fråga 3 av 3.\nVilken är den minsta planeten i solsystemet? \n1. Venus  2. Pluto  3. Merkurius");
+string qwer = Console.ReadLine();
+int qwe = int.Parse(qwer);
+
+if (qwe == 1)
+{
+    Console.WriteLine("Fel svar, ingen poäng.\n");
+}
+else if (qwe == 2)
+{
+    Console.WriteLine("Fel svar, ingen poäng.\n");
+}
+else if (qwe == 3)
+{
+    Console.WriteLine("Rätt svar, en poäng.\n");
+    sum ++;
+}
+else
+{
+    Console.WriteLine("Invalid Response\n");
 }
 
 
 
 Console.WriteLine($"summan blir {sum}");
 
-if (sum > 3)
+if (sum == 3)
 {
     Console.WriteLine("yay, full pott!!!");
 }
@@ -81,5 +104,7 @@ else if (sum == 0)
 {
     Console.WriteLine("Plugga lite och kom tillbaks");
 }
+
+Console.WriteLine("Tryck enter för att stänga spelet.");
 
 Console.ReadLine();
